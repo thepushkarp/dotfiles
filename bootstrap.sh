bb738426cf7dd68908f4982dc502a91d033838e8
Pushkar Patel <thepushkarp@gmail.com>
2020-05-02 17:05:05 +0530

    Add bootstrap file and .editorconfig, separate vim plugins

---
+++ bootstrap.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+# echo "Fetching most recent version..."
+# git pull origin master;
+
+function doIt() {
+    declare -a FILES
+    declare -a NVIMFILES
+
+    FILES=(
+        editorconfig
+        gitconfig
+        gitignore_global
+        gitmessage
+        tmux.conf
+        zshrc
+        p10k.zsh
+    )
+
+    NVIMFILES=(
+        init.vim
+        plugins.vim
+    )
+
+    for FILE in ${FILES[@]}; do
+
+        CURR_FILE=./$FILE
+        DEST_FILE=$HOME/.$FILE
+
+        if [ -f "$DEST_FILE" ]; then
+            BCK_FILE=$DEST_FILE.bak
+            echo "Backing up existing $DEST_FILE to $BCK_FILE"
+            cp -L $DEST_FILE $BCK_FILE
+            rm $DEST_FILE
+            ln $CURR_FILE $DEST_FILE
+        else
+            ln $CURR_FILE $DEST_FILE
+        fi
+
+    done
+
+for FILE in ${NVIMFILES[@]}; do
+
+        CURR_FILE=./$FILE
+        DEST_FILE=$HOME/.config/nvim/$FILE
+
+        if [ -f "$DEST_FILE" ]; then
+            BCK_FILE=$DEST_FILE.bak
+            echo "Backing up existing $DEST_FILE to $BCK_FILE"
+            cp -L $DEST_FILE $BCK_FILE
+            rm $DEST_FILE
+            ln $CURR_FILE $DEST_FILE
+        else
+            ln $CURR_FILE $DEST_FILE
+        fi
+
+    done
+
+    echo "Done!"
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+
+unset doIt;
--- init.vim
+++ init.vim
@@ -2,49 +2,8 @@
 
 " This file will be syntax highlighted in Vim.
 
-let g:plugged_home = '~/.vim/plugged'
-
-" Plugins List
-call plug#begin(g:plugged_home)
-
-  " UI related
-  Plug 'chriskempson/base16-vim'
-  Plug 'vim-airline/vim-airline'
-  Plug 'vim-airline/vim-airline-themes'
-
-  " Better Visual Guide
-  Plug 'Yggdroot/indentLine'
-
-  " syntax check
-  Plug 'w0rp/ale'
-
-  " Autocomplete
-  Plug 'ncm2/ncm2'
-  Plug 'roxma/nvim-yarp'
-  Plug 'ncm2/ncm2-bufword'
-  Plug 'ncm2/ncm2-path'
-
-  " Formater
-  Plug 'Chiel92/vim-autoformat'
-
-  " Editorconfig
-  Plug 'editorconfig/editorconfig-vim'
-
-  " Colour Scheme
-  Plug 'drewtempelmeyer/palenight.vim'
-
-  " Vim Tmux Navigator
-  Plug 'christoomey/vim-tmux-navigator'
-
-  " NerdTree
-  Plug 'scrooloose/nerdtree'
-
-  " Git
-  Plug 'tpope/vim-fugitive'
-  Plug 'airblade/vim-gitgutter'
-  Plug 'Xuyuanp/nerdtree-git-plugin'
-
-call plug#end()
+" Plugins file
+source ~/.config/nvim/plugins.vim
 
 filetype plugin indent on
 
